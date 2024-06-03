5820e20ee071fb35735842092ad40bab000199a1
bpoongs <bpoongs@gmail.com>
2024-06-03 20:01:04 +0530

    indexes autoincrement

--- SQL - Constraints.sql
+++ SQL - Constraints.sql
@@ -1,3 +1,11 @@
+-- contraints list
+-- 1. not null
+-- 2. unique
+-- 3. primary key
+-- 4. foreign key
+-- 5. default
+-- 6. check
+
 CREATE TABLE person_details (
     person_id INT,
     person_firstname VARCHAR(20),
@@ -36,9 +44,9 @@ CREATE TABLE person_details (
 
 -- unique constraint
 -- 1. using alter table query
-alter table person_details add unique(person_mobile_no);
-alter table person_details add constraint UK_person_mobile unique(person_mobile_no);
-insert into person_details values(101, 'Tom', 'Cruise', '1962-10-10', 1234567890, 'NY'); -- error
+ALTER TABLE person_details ADD UNIQUE(person_mobile_no);
+ALTER TABLE person_details ADD CONSTRAINT UK_person_mobile UNIQUE(person_mobile_no);
+INSERT INTO person_details VALUES(101, 'Tom', 'Cruise', '1962-10-10', 1234567890, 'NY'); -- error
 
 -- 2. using create table query
 -- specifying constraint at column level
@@ -73,8 +81,8 @@ CREATE TABLE person_details (
 
 -- primary key constraint
 -- 1. using alter table query
-alter table person_details add primary key(person_id);
-alter table person_details add constraint PK_person_id primary key(person_id);
+ALTER TABLE person_details ADD PRIMARY KEY(person_id);
+ALTER TABLE person_details ADD CONSTRAINT PK_person_id PRIMARY KEY(person_id);
 
 -- 2. using create table query
 -- specifying constraint at column level
@@ -117,26 +125,26 @@ CREATE TABLE employee (
     emp_name VARCHAR(40) NOT NULL,
     emp_contact VARCHAR(10),
     emp_dept_id INT,
-    foreign key(emp_dept_id) references department(dept_id)
+    FOREIGN KEY(emp_dept_id) REFERENCES department(dept_id)
 );
 
-insert into department values(501, 'HR');
-insert into department values(502, 'ADMIN');
-insert into department values(503, 'IT');
-select * from department;
+INSERT INTO department VALUES(501, 'HR');
+INSERT INTO department VALUES(502, 'ADMIN');
+INSERT INTO department VALUES(503, 'IT');
+SELECT * FROM department;
 
-insert into employee values(101, 'abc', '1234567890', 501);
-insert into employee values(102, 'xyz', '888888888', 502);
-insert into employee values(103, 'lmn', '222222222', 503);
-insert into employee values(104, 'asd', '111111111', 503);
-select * from employee;
-insert into employee values(105, 'qwe', '99999999', 567); -- error foreign key violation
+INSERT INTO employee VALUES(101, 'abc', '1234567890', 501);
+INSERT INTO employee VALUES(102, 'xyz', '888888888', 502);
+INSERT INTO employee VALUES(103, 'lmn', '222222222', 503);
+INSERT INTO employee VALUES(104, 'asd', '111111111', 503);
+SELECT * FROM employee;
+INSERT INTO employee VALUES(105, 'qwe', '99999999', 567); -- error foreign key violation
 
 -- default constraint
 -- 1. using alter table query
-alter table person_details alter person_city set default 'world';
-insert into person_details(person_id, person_firstname, person_lastname, person_dob) values (109, 'Jennifer', 'Lopez', '1970-03-03');
-select * from person_details;
+ALTER TABLE person_details ALTER person_city SET DEFAULT 'world';
+INSERT INTO person_details(person_id, person_firstname, person_lastname, person_dob) VALUES (109, 'Jennifer', 'Lopez', '1970-03-03');
+SELECT * FROM person_details;
 -- 2. using create table query
 CREATE TABLE person_details (
     person_id INT,
@@ -147,3 +155,43 @@ CREATE TABLE person_details (
     person_city VARCHAR(30) NOT NULL DEFAULT 'world',
     CONSTRAINT PK_person_id PRIMARY KEY(person_id)
 );
+
+-- check constraint
+-- 1. using alter table query
+ALTER TABLE person_details ADD CHECK(person_mobile_no >= 1000000000 AND person_mobile_no <= 9999999999);
+SELECT * FROM person_details;
+INSERT INTO person_details(person_id, person_firstname, person_lastname, person_dob, person_mobile_no, person_city) 
+	VALUES (101, 'Emma', 'Watson', '1990-03-03', 1564564564, 'Chicago'); -- no error
+INSERT INTO person_details(person_id, person_firstname, person_lastname, person_dob, person_mobile_no, person_city) 
+	VALUES (102, 'Rupert', 'Grint', '2001-03-03', 1564, 'Chicago'); -- error as check constraint is violated
+-- 2. using create table query
+
+
+
+
+-- indexes
+-- primary key constraint = not null constraint + unique constraint + automatically indexed
+SELECT * FROM person_details;
+SELECT * FROM person_details WHERE person_city = 'NY';
+DESC person_details;
+
+CREATE INDEX idx_person_city ON person_details(person_city);
+
+SELECT * FROM person_details WHERE person_city = 'Chicago' AND person_last_name = "Smith";
+
+CREATE INDEX idx_person_city_person_lastname ON person_details(person_city, person_lastname);
+
+ALTER TABLE person_details DROP INDEX idx_person_city_person_lastname;
+
+-- auto increment
+
+CREATE TABLE color_details (
+    color_id INT PRIMARY KEY AUTO_INCREMENT,
+    color_name VARCHAR(20)
+);
+
+insert into color_details(color_name) values ('RED');
+insert into color_details(color_name) values ('BLUE');
+insert into color_details(color_name) values ('GREEN');
+
+select * from color_details;
